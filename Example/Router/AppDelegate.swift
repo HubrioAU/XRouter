46cca79d7c8c4816b8c6b54b6f20d8d96bcf29c0
Reece Como <reece.como@healthengine.com.au>
2019-01-05 23:46:43 +0800

    update to swift 4.2

--- Example/Router/AppDelegate.swift
+++ Example/Router/AppDelegate.swift
@@ -26,7 +26,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     static let homeViewController = ColoredViewController(color: .green, title: "home")
 
     /// Setup app
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = UINavigationController(rootViewController: AppDelegate.homeViewController)
